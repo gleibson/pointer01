0f319c27f780c72ec05173e830ae8389428d7874
glashunti <gleibsonglas2@yahoo.com.br>
2017-04-22 21:43:26 -0300

    First commit

---
+++ pointer01cpp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+
+	int update = 6;
+	int *p_update;
+
+
+	std::cout << "Address update = " << &update << std::endl;
+	std::cout << "Address p_uptade = " << &p_update << std::endl; 
+
+	return 0;
+
+
+
+
+}
